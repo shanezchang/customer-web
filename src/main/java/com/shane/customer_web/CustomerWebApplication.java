8f0bd9715de4252c894b16958427ff56bdf92ff5
shane <shane.z.chang@gmail.com>
2025-02-22 18:51:19 +0800

    feat: gateway转发

--- src/main/java/com/shane/customer_web/CustomerWebApplication.java
+++ src/main/java/com/shane/customer_web/CustomerWebApplication.java
@@ -2,8 +2,10 @@ package com.shane.customer_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerWebApplication {
 
 	public static void main(String[] args) {
