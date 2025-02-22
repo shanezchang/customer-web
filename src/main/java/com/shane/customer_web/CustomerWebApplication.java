712a733cb4879db128048a65bc8ca8b9ee4b5217
shane <shane.z.chang@gmail.com>
2025-02-22 16:04:08 +0800

    feat: init

---
+++ src/main/java/com/shane/customer_web/CustomerWebApplication.java
@@ -0,0 +1,13 @@
+package com.shane.customer_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerWebApplication.class, args);
+	}
+
+}
